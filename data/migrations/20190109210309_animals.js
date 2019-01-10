eb21f5a957fb583fb469cade1e42c3c299e8fddc
Julie Jonak <juliejonak10@gmail.com>
2019-01-09 21:07:05 -0600

    Add species and animal migrations

---
+++ data/migrations/20190109210309_animals.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('animals', table => {
+      table.increments();
+      table.string('Animal').notNullable();
+      table.integer('species_id').unsigned().unique();
+      table.foreign('species_id').references('id').on('species');
+      table.integer('zoo_id').unsigned().unique();
+      table.foreign('zoo_id').references('id').on('zoos');
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('animals');
+  };
